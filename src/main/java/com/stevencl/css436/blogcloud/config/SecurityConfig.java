91e544b550ced4fba0c5211bf19517d32470eb7e
stevecl5 <steveleighton5@gmail.com>
2021-03-13 23:08:19 -0800

    added event listener for auth success

--- src/main/java/com/stevencl/css436/blogcloud/config/SecurityConfig.java
+++ src/main/java/com/stevencl/css436/blogcloud/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.stevencl.css436.blogcloud.config;
 
+import com.stevencl.css436.blogcloud.model.Blog;
+import com.stevencl.css436.blogcloud.model.BlogRepository;
+
 import com.azure.spring.autoconfigure.b2c.AADB2COidcLoginConfigurer;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 /**
  * https://docs.microsoft.com/en-us/azure/developer/java/spring-framework/configure-spring-boot-starter-java-app-with-azure-active-directory-b2c-oidc
@@ -12,18 +18,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AADB2COidcLoginConfigurer configurer;
+    private final BlogRepository blogRepository;
 
-    public SecurityConfig(AADB2COidcLoginConfigurer configurer) {
+    public SecurityConfig(AADB2COidcLoginConfigurer configurer, BlogRepository blogRepository) {
         this.configurer = configurer;
+        this.blogRepository = blogRepository;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/account", "/test/**").authenticated()
+                .antMatchers("/myblog/**").authenticated()
+                .antMatchers("/test/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .apply(configurer);
+    }
+
+    @EventListener
+    public void authSuccessEventListener(AuthenticationSuccessEvent authorizedEvent) {
+        System.out.println("auth success");
+        var user = (OAuth2User) authorizedEvent.getAuthentication().getPrincipal();
+        var oid = user.getName();
+        var displayName = (String) user.getAttribute("name");
+        // create blog for new user
+        if (user.getAttributes().containsKey("newUser")) {
+            var blog = new Blog(oid, displayName);
+            blogRepository.save(blog);
+            System.out.println("created blog: " + oid);
+        } else {
+            // update blog author if display name is different
+            var result = blogRepository.findById(oid);
+            var blog = result.orElse(null);
+            if (blog != null && displayName != null && !displayName.equals(blog.getAuthor())) {
+                blog.setAuthor(displayName);
+                blogRepository.save(blog);
+            }
+        }
     }
 
 }
